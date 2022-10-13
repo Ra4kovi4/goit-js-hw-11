9120c17fc1dfa76a59dd7e555a81df1e942bec5b
Kristina Rachkovich <ra4kovi4@ukr.net>
2022-10-13 20:52:46 +0300

    finished task

---
+++ src/js/fetch_image.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+const axios = require('axios').default;
+
+export default class ImageFetchApi {
+  constructor() {
+    this.searchQuery = '';
+
+    this.page = 1;
+  }
+  async getImage() {
+    axios.defaults.baseURL = 'https://pixabay.com/api';
+    const searchParams = new URLSearchParams({
+      key: '29880081-a66e36470932d0d43dbaf5012',
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: this.page,
+    });
+    const { data } = await axios.get(`/?${searchParams}`);
+
+    return data;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
